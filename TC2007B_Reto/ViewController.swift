aa7353ee3916dc47006b3e9628480fc9afdd58b8
ValeriaMorenoE <a01280603@itesm.mx>
2021-08-24 22:30:31 -0500

    Initial Commit

---
+++ TC2007B_Reto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TC2007B_Reto
+//
+//  Created by Valeria Moreno on 24/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
