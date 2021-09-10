682e3943deccb6468f5730fe1359825ff438f2a1
ValeriaMorenoE <a01280603@itesm.mx>
2021-09-10 17:10:46 -0500

    Historial

--- TC2007B_Reto/MenuViewController.swift
+++ TC2007B_Reto/MenuViewController.swift
@@ -57,7 +57,7 @@ class MenuViewController: UIViewController {
         let museo3dStoryboard = UIStoryboard(name: "Museo3d", bundle: nil)
         let vc = museo3dStoryboard.instantiateViewController(identifier: "Museo3dViewController") as! Museo3dViewController
         
-        vc.modalPresentationStyle = .currentContext
+        //vc.modalPresentationStyle = .currentContext
         self.present(vc, animated: true, completion: nil)
     }
     @IBAction func misComprasButtonPressed(_ sender: Any) {
