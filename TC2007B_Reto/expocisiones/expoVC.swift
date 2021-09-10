b62a33b7d86df36efb7bde74610f3ce507f50d59
Ruben Robles <rroblesleal@hotmail.com>
2021-09-10 16:50:00 -0500

    tus cambios david
    tus cambios david
    
    PS: tus cambios david

---
+++ TC2007B_Reto/expocisiones/expoVC.swift
@@ -0,0 +1,29 @@
+//
+//  expoVC.swift
+//  TC2007B_Reto
+//
+//  Created by user190339 on 9/9/21.
+//
+
+import UIKit
+
+class expoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
