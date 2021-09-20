a00e25cc4454922e62378b7827e29b657b1f694e
ValeriaMorenoE <a01280603@itesm.mx>
2021-09-20 10:50:22 -0500

    Museo 3d regresa a menu principal

--- TC2007B_Reto/Museo3dViewController.swift
+++ TC2007B_Reto/Museo3dViewController.swift
@@ -8,5 +8,13 @@
 import UIKit
 
 class Museo3dViewController: UIViewController {
+    @IBOutlet weak var menuButton: UIButton!
     
+    @IBAction func goBack(_ sender: Any) {
+        let homeStoryBoard = UIStoryboard(name: "Menu", bundle: nil)
+        let vc = homeStoryBoard.instantiateViewController(identifier: "MainMenuViewController") as! MenuViewController
+        
+        vc.modalPresentationStyle = .currentContext
+        self.present(vc, animated: true, completion: nil)
+    }
 }
